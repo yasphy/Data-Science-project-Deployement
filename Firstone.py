d2aea964b12018fba2efaba12e7712bc3baf7db9
yasphy <78342519+yasphy@users.noreply.github.com>
2021-08-11 09:04:56 +0530

    Update Firstone.py

--- Firstone.py
+++ Firstone.py
@@ -5,9 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 Cafes=pd.read_csv("https://raw.githubusercontent.com/yasphy/Data-Science-project-Deployement/main/Data%20Test%20-%20Sheet1.csv")
-st.title("Least travelled cafes")
+st.title("Nearest cafes and salons")
 st.image("https://media-cdn.tripadvisor.com/media/photo-s/15/ba/b1/f4/2-18-largejpg.jpg")
-st.write("Given you chose a cafe from given cafe list, you can get 12 nearest cafes")
+st.write("Given you chose a cafe/salon from given cafe and salon list, you can get 12 nearest cafes as well as salons")
 #Displaying data
 st.write("# Displaying Data")
 #display dataframe
@@ -15,7 +15,7 @@ st.write("# Displaying Data")
 col_names = Cafes.columns.tolist()
 st.dataframe(Cafes[st.multiselect("Columns to display",col_names, default = col_names)])
 ###Plotting
-st.write("# Plotting")
+st.write("# Plotting ratings")
 ##display figures
 fig,ax = plt.subplots() #must create a subplot
 ax = sns.countplot(Cafes["Rating"], palette ="tab20")
@@ -52,13 +52,13 @@ for m in range(0,339):
 k.sort()
 p=[]
 for w in range(0,339):
-    for q in range(0,12):
+    for q in range(0,13):
         if (k[q]==distance(Cafes["Latitude"][w],Cafes["Latitude"][i[0]],Cafes["Longitude"][w],Cafes["Longitude"][i[0]])):
             p.append(Cafes["Company Name"][w])
 res = []
 [res.append(x) for x in p if x not in res]
 st.markdown('**__The twelve nearest cafes are__**')
-for ik in range(1,13):
+for ik in range(1,len(res)):
 	st.write(res[ik])
 
 # Streamlit widgets automatically run the script from top to bottom. Since
